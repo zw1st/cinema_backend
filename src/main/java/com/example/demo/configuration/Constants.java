d4c12e647dfbcd94eda98ebd1c4fa5d15ff640a3
alex <saneknastile@gmail.com>
2026-03-14 20:15:20 +0400

    init

---
+++ src/main/java/com/example/demo/configuration/Constants.java
@@ -0,0 +1,9 @@
+package com.example.demo.configuration;
+
+public class Constants {
+    public static final String DEV_ORIGIN = "http://localhost:5173";
+    public static final String API_URL = "/api/1.0";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry
+                .addMapping(Constants.API_URL + "/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(Constants.DEV_ORIGIN);
+    }
+}
